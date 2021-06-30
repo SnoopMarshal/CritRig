ba86758b67ab9765d8a39e153d3b690acde90dde
Bikram Nath <bikramnath.office@gmail.com>
2021-07-01 00:35:23 +0530

    removed semantic ui

--- src/components/RigCard/index.jsx
+++ src/components/RigCard/index.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React from "react";
 
 export default function RigCard() {
-    return (
-        <div class="ui cards"><div class="ui card"><div class="content"><div class="header">Project Report - April</div><div class="meta">ROI: 30%</div><div class="description">Leverage agile frameworks to provide a robust synopsis for high level overviews.</div></div></div><div class="ui card"><div class="content"><div class="header">Project Report - May</div><div class="meta">ROI: 34%</div><div class="description">Bring to the table win-win survival strategies to ensure proactive domination.</div></div></div><div class="ui card"><div class="content"><div class="header">Project Report - June</div><div class="meta">ROI: 27%</div><div class="description">Capitalise on low hanging fruit to identify a ballpark value added activity to beta test.</div></div></div></div>
-    )
+  return (
+    <div class="ui cards">
+      <div class="ui card">
+        <div class="content">
+          <div class="header">Project Report - April</div>
+          <div class="meta">ROI: 30%</div>
+          <div class="description">
+            Leverage agile frameworks to provide a robust synopsis for high
+            level overviews.
+          </div>
+        </div>
+      </div>
+      <div class="ui card">
+        <div class="content">
+          <div class="header">Project Report - May</div>
+          <div class="meta">ROI: 34%</div>
+          <div class="description">
+            Bring to the table win-win survival strategies to ensure proactive
+            domination.
+          </div>
+        </div>
+      </div>
+      <div class="ui card">
+        <div class="content">
+          <div class="header">Project Report - June</div>
+          <div class="meta">ROI: 27%</div>
+          <div class="description">
+            Capitalise on low hanging fruit to identify a ballpark value added
+            activity to beta test.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
