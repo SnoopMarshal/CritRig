0d7a3dc45c6ed2c79a3b801cd3b6fa380b40df22
Bikram Nath <bikramnath.office@gmail.com>
2021-07-01 00:30:23 +0530

    initialisation

---
+++ src/components/App/index.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import './App.css'
+import RigCard from "./../RigCard";
+
+function App() {
+
+  return (
+    <div className="main">
+      <RigCard />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/RigCard/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function RigCard() {
+    return (
+        <div class="ui cards"><div class="ui card"><div class="content"><div class="header">Project Report - April</div><div class="meta">ROI: 30%</div><div class="description">Leverage agile frameworks to provide a robust synopsis for high level overviews.</div></div></div><div class="ui card"><div class="content"><div class="header">Project Report - May</div><div class="meta">ROI: 34%</div><div class="description">Bring to the table win-win survival strategies to ensure proactive domination.</div></div></div><div class="ui card"><div class="content"><div class="header">Project Report - June</div><div class="meta">ROI: 27%</div><div class="description">Capitalise on low hanging fruit to identify a ballpark value added activity to beta test.</div></div></div></div>
+    )
+}
